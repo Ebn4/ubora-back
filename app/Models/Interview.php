e3d6f2329eba1db3e3d9c750f5dbd2d91a7bf804
zgeniusecoders <zgeniusecoders@gmail.com>
2025-06-25 13:01:38 +0100

    feat: création modele interview

---
+++ app/Models/Interview.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Interview extends Model
+{
+    protected $fillable = [
+        'candidacy_id',
+    ];
+
+    public function candidacy()
+    {
+        return $this->belongsTo(Candidacy::class);
+    }
+
+}
