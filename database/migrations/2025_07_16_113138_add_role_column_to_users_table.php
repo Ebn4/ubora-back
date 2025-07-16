5330889d20cf87d244efb42ebfd443b22174f616
zgeniusecoders <zgeniusecoders@gmail.com>
2025-07-16 13:05:29 +0100

    feat: ajout de la colonne role dans la table user

---
+++ database/migrations/2025_07_16_113138_add_role_column_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Enums\RoleEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', array_column(RoleEnum::cases(), 'value'))
+                ->after('name')
+                ->default(RoleEnum::EVALUATOR->value);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+};
