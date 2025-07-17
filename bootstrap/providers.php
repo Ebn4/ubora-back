51791814a3a128241960b3685f8cfe1ed2e059e5
Dibakala-Muke <dibakala90@gmail.com>
2025-07-17 12:49:10 +0100

    Suppression de ReadExcelFileServiceProvider dans providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,7 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthenticationServiceProvider::class,
     App\Providers\FileServiceProvider::class,
-    App\Providers\ReadExcelFileServiceProvider::class,
     App\Providers\UserServiceProvider::class,
     App\Providers\UserLdapServiceProvider::class,
     App\Providers\EvaluatorServiceProvider::class
