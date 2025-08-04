5e92eb13747067f00c8c8d9c04886cfa19ef65e1
zgeniusecoders <zgeniusecoders@gmail.com>
2025-08-04 10:56:32 +0100

    feat: ajout du status STATUS_CLOSE dans PeriodStatusEnum.php

--- app/Enums/PeriodStatusEnum.php
+++ app/Enums/PeriodStatusEnum.php
@@ -5,7 +5,7 @@ namespace App\Enums;
 enum PeriodStatusEnum: string
 {
     case STATUS_DISPATCH = 'DISPATCH';
-    case STATUS_INTERVIEW = 'INTERVIEW';
+    case STATUS_CLOSE = 'CLOSE';
     case STATUS_PRESELECTION = 'PRESELECTION';
     case STATUS_SELECTION = 'SELECTION';
 }
